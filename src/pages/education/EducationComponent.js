3c303f2b9fbe24b2bd1ea09abf360c22991b9672
prayfor.ee <prayfor.ee@gmail.com>
2022-01-21 13:12:27 +0900

    test

--- src/pages/education/EducationComponent.js
+++ src/pages/education/EducationComponent.js
@@ -3,7 +3,10 @@ import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import TopButton from "../../components/topButton/TopButton";
 import Educations from "../../containers/education/Educations";
+import Certifications from "../../containers/certifications/Certifications";
+import CompetitiveSites from "../../components/competitiveSites/CompetitiveSites";
 import EducationImg from "./EducationImg";
+import { competitiveSites } from "../../portfolio";
 import "./EducationComponent.css";
 import { Fade } from "react-reveal";
 
@@ -30,10 +33,12 @@ class Education extends Component {
                 <h3 className="heading-sub-text" style={{ color: theme.text }}>
                   Basic Qualification and Certifcations
                 </h3>
+                <CompetitiveSites logos={competitiveSites.competitiveSites} />
               </div>
             </div>
           </Fade>
           <Educations theme={this.props.theme} />
+          <Certifications theme={this.props.theme} />
         </div>
         <Footer theme={this.props.theme} />
         <TopButton theme={this.props.theme} />
