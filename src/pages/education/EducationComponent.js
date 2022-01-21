f3ea3eec4e81d0ede708f2d1a6691e7afbb45360
prayfor.ee <prayfor.ee@gmail.com>
2022-01-21 12:46:18 +0900

    modified issue unused variable

--- src/pages/education/EducationComponent.js
+++ src/pages/education/EducationComponent.js
@@ -3,10 +3,7 @@ import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import TopButton from "../../components/topButton/TopButton";
 import Educations from "../../containers/education/Educations";
-import Certifications from "../../containers/certifications/Certifications";
-//import CompetitiveSites from "../../components/competitiveSites/CompetitiveSites";
 import EducationImg from "./EducationImg";
-//import { competitiveSites } from "../../portfolio";
 import "./EducationComponent.css";
 import { Fade } from "react-reveal";
 
@@ -37,7 +34,6 @@ class Education extends Component {
             </div>
           </Fade>
           <Educations theme={this.props.theme} />
-          <Certifications theme={this.props.theme} />
         </div>
         <Footer theme={this.props.theme} />
         <TopButton theme={this.props.theme} />
